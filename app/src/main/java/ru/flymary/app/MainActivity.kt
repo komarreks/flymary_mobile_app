35bd78a7b488d8ff3887124cd488b8bdcceda825
komarreks <komarreks@gmail.com>
2024-11-15 13:59:23 +0500

    15.11.2024 - Сделал нижнее меню, наконец то

--- app/src/main/java/ru/flymary/app/MainActivity.kt
+++ app/src/main/java/ru/flymary/app/MainActivity.kt
@@ -1,10 +1,13 @@
 package ru.flymary.app
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import ru.flymary.app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,5 +20,24 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.bottomMenu
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        //val navController = findNavController(R.id.nav_host_fragment_container)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.search_fragment,
+                R.id.search_fragment,
+                R.id.basket_fragment,
+                R.id.profile_fragment,
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
